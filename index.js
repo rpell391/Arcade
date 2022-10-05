416b183ceebb3e25b62024877340c80e93e9b89e
Randy Pellegrin <rpell391@gmail.com>
2022-10-05 12:49:38 -0500

    CSS, HTML, and first event listener

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+//const cells = document.getElementsByClassName("cell");
+
+const gameArea = document.getElementById("board");
+gameArea.addEventListener("click", function (clickEvent) {
+  clickEvent.target.innerText = "X";
+});
